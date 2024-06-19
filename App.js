1d2c21a0189f5aa068ed1fafe9794a055bb323a6
Your Name <you@example.com>
2024-06-19 20:54:43 -0300

    commit

--- App.js
+++ App.js
@@ -1,15 +1,9 @@
 import React from 'react'
 import AppNavigation from './src/navigation/AppNavigation'
-// import AuthNavigation from './src/navigation/AuthNavigation'
-
-// export default function App(){
-//   const [isAuthenticated, setIsAuthenticated] = useState(false)
-
-//   return isAuthenticated ? <AppNavigation /> : <AuthNavigation />
-// }
+import AuthNavigation from './src/navigation/AuthNavigation'
 
 export default function App(){
-  return (
-    <AppNavigation />
-  )
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+
+  return isAuthenticated ? <AppNavigation /> : <AuthNavigation />
 }
